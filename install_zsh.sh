13ec1c58f321b6ad88f2e8527b7b816457a9b7e8
root <root@HERO-CENTER-01.localdomain>
2021-11-25 00:35:36 +0100

    Tweaked install script

--- install_zsh.sh
+++ install_zsh.sh
@@ -10,3 +10,6 @@ rm -rf "$HOME/.oh-my-zsh" 2> /dev/null
 export ZSH="$HOME/.oh-my-zsh"
 export ZSH_CUSTOM="$ZSH/custom/"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Install powerlevel10k.
+rm -rf ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k >/dev/null && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
