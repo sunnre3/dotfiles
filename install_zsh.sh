a495bd8c6b83d2b8d987037d7219430089e46587
root <root@HERO-CENTER-01.localdomain>
2021-11-25 00:03:41 +0100

    Tweaked install script

--- install_zsh.sh
+++ install_zsh.sh
@@ -7,6 +7,6 @@ fi
 
 # Install oh-my-zsh.
 export ZSH="$HOME/.oh-my-zsh"
-sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
 
 sudo cp /root/.oh-my-zsh $HOME 2> /dev/null
